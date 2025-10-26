423b7baa520ca77b04888a5be819abcc0e6fc13c
Shehla-dinc <sehladinc1903@gmail.com>
2025-10-26 21:25:45 +0300

    Add files via upload

---
+++ 5.hafta Hanoi Kuleleri.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Hanoi(int n, char kaynak, char hedef, char ara)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"{kaynak} -> {hedef}");
+            return;
+        }
+
+        Hanoi(n - 1, kaynak, ara, hedef);
+        Console.WriteLine($"{kaynak} -> {hedef}");
+        Hanoi(n - 1, ara, hedef, kaynak);
+    }
+
+    static void Main()
+    {
+        Console.Write("Disk sayısını giriniz: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("\nAdım adım hareketler:\n");
+        Hanoi(n, 'A', 'C', 'B');
+    }
+}
